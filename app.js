6b1e211a686adab89420d5c2d575b2f503105c6b
Abhay0122 <parmarabhay2201@gmail.com>
2022-12-18 23:45:55 +0530

    changes

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value": 400,
+        "value": 300,
         "density": {
           "enable": true,
           "value_area": 800
